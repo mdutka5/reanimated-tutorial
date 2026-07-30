4b713498ffb69d17a025869400f06308ab94af2e
Marcin Dutka <marcin.dutka.05@gmail.com>
2026-07-30 15:25:29 +0200

    ex. 02 -> animated SVG

--- app/(tabs)/02.tsx
+++ app/(tabs)/02.tsx
@@ -1,9 +1,61 @@
-import { View, Text } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedProps,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { Path, Svg } from "react-native-svg";
+import { useWindowDimensions } from "react-native";
 
-export default function SettingsScreen() {
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
+const AnimatedPath = Animated.createAnimatedComponent(Path);
+
+export default function screen02() {
+  const { width, height } = useWindowDimensions();
+  const cx = useSharedValue(width / 2);
+  const cy = useSharedValue(height / 2);
+  const r = useSharedValue(100);
+
+  const LINE = `M ${cx.value - 100} ${cy.value} L ${cx.value + 100} ${cy.value}`;
+  //const CIRCLE = `M ${cx.value},${cy.value - r.value} A ${r.value},${r.value} 0 1,1 ${cx.value},${cy.value + r.value} A ${r.value},${r.value} 0 1,1 ${cx.value},${cy.value - r.value}`;
+  //const CIRCLE = `M ${cx.value},${cy.value - r.value} A ${r.value},${r.value} 0 1,0 ${cx.value},${cy.value + r.value} A ${r.value},${r.value} 0 1,0 ${cx.value},${cy.value - r.value}`;
+  //const CIRCLE = `M ${cx.value},${cy.value + r.value} A ${r.value},${r.value} 0 1,0 ${cx.value},${cy.value - r.value} A ${r.value},${r.value} 0 1,0 ${cx.value},${cy.value + r.value}`;
+  const CIRCLE = `M ${cx.value},${cy.value + r.value} A ${r.value},${r.value} 0 1,1 ${cx.value},${cy.value - r.value} A ${r.value},${r.value} 0 1,1 ${cx.value},${cy.value + r.value}`;
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Settings Tab</Text>
+    <View style={styles.container}>
+      <AnimatedSvg
+        width={width}
+        height={height}
+        style={StyleSheet.absoluteFill}
+      >
+        <AnimatedPath
+          animatedProps={{
+            animationName: {
+              from: { d: LINE },
+              to: { d: CIRCLE },
+            },
+            animationDuration: "2s",
+            animationIterationCount: "infinite",
+            animationDirection: "alternate",
+            animationTimingFunction: "ease-in-out",
+          }}
+          stroke="red"
+          strokeWidth={4}
+        />
+      </AnimatedSvg>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  buttonContainer: {
+    marginBottom: 40,
+  },
+});
